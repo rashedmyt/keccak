227296bf894f40b7b0fecb612849664af5303bc6
rashedmyt <25506588+rashedmyt@users.noreply.github.com>
2019-01-14 16:15:44 +0530

    no classes only functions

--- lib/keccak.dart
+++ lib/keccak.dart
@@ -2,131 +2,122 @@
 //
 // Please see the included LICENSE file for more information.
 
+/// pre-NIST implementation of keccak generally used by cryptocurrencies.
 library keccak;
 
 import 'dart:typed_data';
 import 'dart:math';
 import 'src/constants.dart';
 
-class Keccak {
-  static int _logicalShiftRight(int val, int n) =>
-      (val >> n) & ~(-1 << (64 - n));
+int _logicalShiftRight(int val, int n) => (val >> n) & ~(-1 << (64 - n));
 
-  static int _rotl64(int x, int y) {
-    return ((x << y) | _logicalShiftRight(x, (64 - y)));
-  }
+int _rotl64(int x, int y) {
+  return ((x << y) | _logicalShiftRight(x, (64 - y)));
+}
 
-  static void _keccakf(Uint64List st, [int rounds = KECCAK_ROUNDS]) {
-    int t;
-    Uint64List bc = Uint64List(5);
+void _keccakf(Uint64List st, [int rounds = KECCAK_ROUNDS]) {
+  int t;
+  Uint64List bc = Uint64List(5);
 
-    for (int round = 0; round < rounds; round++) {
-      // Theta
-      for (int i = 0; i < 5; i++) {
-        bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
-      }
+  for (int round = 0; round < rounds; round++) {
+    // Theta
+    for (int i = 0; i < 5; i++) {
+      bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
+    }
 
-      for (int i = 0; i < 5; i++) {
-        t = bc[(i + 4) % 5] ^ _rotl64(bc[(i + 1) % 5], 1);
-        for (int j = 0; j < 25; j += 5) {
-          st[j + i] ^= t;
-        }
-      }
-
-      // Rho Pi
-      t = st[1];
-      for (int i = 0; i < 24; i++) {
-        int j = keccakPiln[i];
-        bc[0] = st[j];
-        st[j] = _rotl64(t, keccakRotc[i]);
-        t = bc[0];
-      }
-
-      // Chi
+    for (int i = 0; i < 5; i++) {
+      t = bc[(i + 4) % 5] ^ _rotl64(bc[(i + 1) % 5], 1);
       for (int j = 0; j < 25; j += 5) {
-        for (int i = 0; i < 5; i++) {
-          bc[i] = st[j + i];
-        }
-        for (int i = 0; i < 5; i++) {
-          st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
-        }
+        st[j + i] ^= t;
       }
-
-      // Iota
-      st[0] ^= keccakRoundConstants[round];
     }
+
+    // Rho Pi
+    t = st[1];
+    for (int i = 0; i < 24; i++) {
+      int j = keccakPiln[i];
+      bc[0] = st[j];
+      st[j] = _rotl64(t, keccakRotc[i]);
+      t = bc[0];
+    }
+
+    // Chi
+    for (int j = 0; j < 25; j += 5) {
+      for (int i = 0; i < 5; i++) {
+        bc[i] = st[j + i];
+      }
+      for (int i = 0; i < 5; i++) {
+        st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+      }
+    }
+
+    // Iota
+    st[0] ^= keccakRoundConstants[round];
   }
+}
 
-  /*
-    Compute a hash of length outputSize from input 
-  */
-  static Uint8List _keccak(Uint8List input, int outputSize) {
-    Uint64List st = Uint64List(25);
-    Uint8List temp = Uint8List(144);
-    ByteData inp = input.buffer.asByteData();
+// Compute a hash of length outputSize from input
+Uint8List _keccak(Uint8List input, int outputSize) {
+  Uint64List st = Uint64List(25);
+  Uint8List temp = Uint8List(144);
+  ByteData inp = input.buffer.asByteData();
 
-    int inlen = input.length;
-    int offset = 0; // Offset of input array
+  int inlen = input.length;
+  int offset = 0;
 
-    int rsiz, rsizw;
+  int rsiz, rsizw;
 
-    rsiz =
-        st.lengthInBytes == outputSize ? HASH_DATA_AREA : 200 - 2 * outputSize;
-    rsizw = rsiz ~/ 8;
+  rsiz = st.lengthInBytes == outputSize ? HASH_DATA_AREA : 200 - 2 * outputSize;
+  rsizw = rsiz ~/ 8;
 
-    for (; inlen >= rsiz; inlen -= rsiz, offset += rsiz) {
-      for (int i = 0; i < rsizw; i++) {
-        st[i] ^= inp.getUint64(offset + (i * 8), Endian.host);
-      }
-      _keccakf(st, KECCAK_ROUNDS);
-    }
-
-    for (int i = 0; i < inlen; i++) {
-      temp[i] = input[offset + i];
-    }
-    temp[inlen++] = 1;
-    for (int i = 0; i < rsiz - inlen; i++) {
-      temp[inlen + i] = 0;
-    }
-    temp[rsiz - 1] |= 0x80;
-
-    ByteData tempData = temp.buffer.asByteData();
+  for (; inlen >= rsiz; inlen -= rsiz, offset += rsiz) {
     for (int i = 0; i < rsizw; i++) {
-      st[i] ^= tempData.getUint64(i * 8, Endian.host);
+      st[i] ^= inp.getUint64(offset + (i * 8), Endian.host);
     }
-
     _keccakf(st, KECCAK_ROUNDS);
-
-    Uint8List output = st.buffer.asUint8List(0, outputSize);
-
-    return output;
   }
 
-  /* 
-    Hashes the given input with keccak, into an output hash of 200 bytes. 
-  */
-  static Uint8List keccak1600(Uint8List input) {
-    return _keccak(input, 200);
+  for (int i = 0; i < inlen; i++) {
+    temp[i] = input[offset + i];
+  }
+  temp[inlen++] = 1;
+  for (int i = 0; i < rsiz - inlen; i++) {
+    temp[inlen + i] = 0;
+  }
+  temp[rsiz - 1] |= 0x80;
+
+  ByteData tempData = temp.buffer.asByteData();
+  for (int i = 0; i < rsizw; i++) {
+    st[i] ^= tempData.getUint64(i * 8, Endian.host);
   }
 
-  /* 
-    Hashes the given input with keccak, into an output hash of 32 bytes.
-    Copies outputLength bytes of the output and returns it. Output
-    length cannot be larger than 32.
-  */
-  static Uint8List keccak(Uint8List input, [int outputLength = 32]) {
-    if (outputLength > 32) {
-      throw ArgumentError("Output length must be 32 bytes or less!");
-    }
+  _keccakf(st, KECCAK_ROUNDS);
 
-    Uint8List result = _keccak(input, 32);
+  Uint8List output = st.buffer.asUint8List(0, outputSize);
 
-    Uint8List output = Uint8List(outputLength);
+  return output;
+}
 
-    for (int i = 0; i < min(outputLength, 32); i++) {
-      output[i] = result[i];
-    }
+/// Hashes the given input with keccak, into an output hash of 200 bytes.
+Uint8List keccak1600(Uint8List input) {
+  return _keccak(input, 200);
+}
 
-    return output;
+/// Hashes the given input with keccak, into an output hash of 32 bytes.
+/// Copies outputLength bytes of the output and returns it. Output
+/// length cannot be larger than 32.
+Uint8List keccak(Uint8List input, [int outputLength = 32]) {
+  if (outputLength > 32) {
+    throw ArgumentError("Output length must be 32 bytes or less!");
   }
+
+  Uint8List result = _keccak(input, 32);
+
+  Uint8List output = Uint8List(outputLength);
+
+  for (int i = 0; i < min(outputLength, 32); i++) {
+    output[i] = result[i];
+  }
+
+  return output;
 }
--- test/keccak_test.dart
+++ test/keccak_test.dart
@@ -10,23 +10,23 @@ import 'package:convert/convert.dart';
 
 void main() {
   test('compute keccak hashes', () {
-    expect(hex.encode(Keccak.keccak(Uint8List.fromList("".codeUnits))),
+    expect(hex.encode(keccak(Uint8List.fromList("".codeUnits))),
         "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
     expect(
-        hex.encode(Keccak.keccak(Uint8List.fromList(
+        hex.encode(keccak(Uint8List.fromList(
             "The quick brown fox jumps over the lazy dog".codeUnits))),
         "4d741b6f1eb29cb2a9b9911c82f56fa8d73b04959d3d9d222895df6c0b28aa15");
     expect(
-        hex.encode(Keccak.keccak(Uint8List.fromList(
+        hex.encode(keccak(Uint8List.fromList(
             "The quick brown fox jumps over the lazy dog.".codeUnits))),
         "578951e24efd62a3d63a86f7cd19aaa53c898fe287d2552133220370240b572d");
     expect(
-        hex.encode(Keccak.keccak(Uint8List.fromList(
+        hex.encode(keccak(Uint8List.fromList(
             "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself"
                 .codeUnits))),
         "d6a63dc2e3ab16360c1dd26fa4b343af9dde6b4ae275793b1d64eaffdc02f1d9");
     expect(
-        hex.encode(Keccak.keccak(Uint8List.fromList([
+        hex.encode(keccak(Uint8List.fromList([
           0,
           1,
           2,
@@ -61,6 +61,6 @@ void main() {
           31,
         ]))),
         "8ae1aa597fa146ebd3aa2ceddf360668dea5e526567e92b0321816a4e895bd2d");
-    expect(() => Keccak.keccak(null), throwsNoSuchMethodError);
+    expect(() => keccak(null), throwsNoSuchMethodError);
   });
 }
